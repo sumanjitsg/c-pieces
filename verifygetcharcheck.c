baef27c5c70ee09c065d304bfa69de91ec4dfedd
Sumanjit SenGupta <sumanjitsengupta08@gmail.com>
2017-01-28 23:37:50 +0530

    Verify that getchar() != EOF is 0 or 1

---
+++ verifygetcharcheck.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* Verify that the expression getchar() != EOF
+ * is 0 or 1
+ * Press Enter or Ctrl+d (EOF) to mark end of stream (Enter is taken as a character)
+ * Press Ctrl+d (EOF) at beginning to come out of loop
+ */
+
+int main()
+{
+	int c;
+
+	while(c = getchar() != EOF)
+		printf("%d\n", c);
+
+	printf("%d\n", c);
+	return 0;
+}
+
