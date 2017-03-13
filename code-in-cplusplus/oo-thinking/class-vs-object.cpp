7644e89d7f0b3e31fb540fbf41d82890bf8f74bf
Sumanjit SenGupta <sumanjitsengupta08@gmail.com>
2017-03-13 23:08:58 +0530

    Class vs Object

---
+++ code-in-cplusplus/oo-thinking/class-vs-object.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+class Dummy {
+	public:
+	    int a;
+	    int b;
+
+	    Dummy(int a, int b);
+	    int sum(void);
+	    int mul(void);
+};
+
+Dummy::Dummy(int a, int b) {
+	this->a = a;
+	this->b = b;
+}
+
+int Dummy::sum() {
+	return (this->a + this->b);
+}
+
+int Dummy::mul() {
+	return (this->a * this->b);
+}
+
+int main() {
+	Dummy* d1 = new Dummy(10, 21);
+	Dummy* d2 = new Dummy(3, 15);
+	Dummy* d3 = new Dummy(14, 9);
+	cout << d1->sum() << endl;
+	cout << d2->sum() << endl;
+	cout << d3->mul() << endl;
+	delete d1, delete d2, delete d3;	
+	return 0;
+}
